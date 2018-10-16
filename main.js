f7c23206616d0281bf42c783de1ea99f9b9a53d8
buffard <samuelwebber19@gmail.com>
2018-10-16 14:21:08 -0500

    Chicken Monkey Exercise

---
+++ main.js
@@ -0,0 +1,15 @@
+// Write a program that prints the numbers from 1 to 100. But for multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number and for the multiples of seven (7, 14, 21, etc.) print "Monkey". For numbers which are multiples of both five and seven print "ChickenMonkey".
+
+// To determine if a number can be evenly divided by 5 or 7, use the JavaScript remainder operator.
+
+for (var i = 1; i <= 100; i++) {
+  if (i % 7 === 0 && i % 5 === 0) {
+    console.log("ChickenMonkey")
+  } else if (i % 5 === 0) {
+    console.log("Chicken")
+  } else if (i % 7 === 0) {
+    console.log("Monkey")
+  } else {
+    console.log(i)
+  }
+}
